6636aa55e1d821c7baac75ffd575817e3695e08b
Olena Shatalova <164022674+OlenaShatalova@users.noreply.github.com>
2024-05-27 18:23:45 +0300

    add animate aos

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-const elementToToggle = document.querySelector(".mobile-backdrop");
-const toggleButton = document.querySelector(".header-mobile-open-btn");
-const closeButton = document.querySelector(".mobile-close-btn");
-const headerMobMenu = document.querySelector(".mobile-menu");
-const linksMenu = document.querySelectorAll(".mobile-nav-link");
+const elementToToggle = document.querySelector('.mobile-backdrop');
+const toggleButton = document.querySelector('.header-mobile-open-btn');
+const closeButton = document.querySelector('.mobile-close-btn');
+const headerMobMenu = document.querySelector('.mobile-menu');
+const linksMenu = document.querySelectorAll('.mobile-nav-link');
 function toggleClass() {
-  elementToToggle.classList.toggle("is-open");
+  elementToToggle.classList.toggle('is-open');
 }
-closeButton.addEventListener("click", toggleClass);
-toggleButton.addEventListener("click", toggleClass);
-elementToToggle.addEventListener("click", toggleClass);
-headerMobMenu.addEventListener("click", e => e.stopImmediatePropagation());
+closeButton.addEventListener('click', toggleClass);
+toggleButton.addEventListener('click', toggleClass);
+elementToToggle.addEventListener('click', toggleClass);
+headerMobMenu.addEventListener('click', e => e.stopImmediatePropagation());
 for (let i = 0; i < linksMenu.length; i++) {
-  linksMenu[i].addEventListener("click", toggleClass);
+  linksMenu[i].addEventListener('click', toggleClass);
 }
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
